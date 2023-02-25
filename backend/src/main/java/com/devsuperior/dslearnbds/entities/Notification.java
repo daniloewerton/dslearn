afa6054f07c6a0170201f45565d273e241ec5517
Danilo Ewerton <danilo.ewe@gmail.com>
2023-02-25 10:53:22 -0300

    Notification/User fix

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
@@ -14,11 +14,14 @@ public class Notification implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String text;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
     private boolean read = false;
     private String route;
 
     @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
     public Notification() {
